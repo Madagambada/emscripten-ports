38193deeb0f14cdec9789d4de330f8bdc159f8af
Madagambada <47931256+Madagambada@users.noreply.github.com>
2024-12-01 20:58:24 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ installdir=$(pwd)/install
 wget https://github.com/openssl/openssl/releases/download/openssl-3.4.0/openssl-3.4.0.tar.gz
 tar -xf openssl-3.4.0.tar.gz
 cd openssl-3.4.0
-emconfigure ./Configure --release -no-tests -no-asm -no-afalgeng -no-apps -no-shared --prefix=$installdir
-emmake make -j4
+emconfigure ./Configure CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' --release -no-tests -no-asm -no-afalgeng -no-apps -no-shared --prefix=$installdir
+emmake make CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' -j4 
 emmake make install
 cd ..
 
@@ -25,8 +25,8 @@ cd ..
 wget https://github.com/nghttp2/nghttp2/releases/download/v1.64.0/nghttp2-1.64.0.tar.gz
 tar -xf nghttp2-1.64.0.tar.gz
 cd nghttp2-1.64.0 
-emconfigure ./configure --enable-lib-only --prefix $installdir
-emmake make -j4
+emconfigure ./configure CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' --enable-lib-only --prefix $installdir
+emmake make CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' -j4
 emmake make install
 cd ..
 
@@ -34,8 +34,8 @@ cd ..
 wget https://github.com/madler/zlib/releases/download/v1.3.1/zlib131.zip
 unzip zlib131.zip
 cd zlib-1.3.1
-emconfigure ./configure --static --prefix $installdir
-emmake make -j4
+emconfigure ./configure CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' --static --prefix $installdir
+emmake make CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' -j4
 emmake make install
 cd ..
 
@@ -45,8 +45,8 @@ tar -xf c-ares-1.34.3.tar.gz
 cd c-ares-1.34.3
 mkdir build
 cd build
-emcmake cmake -DCMAKE_BUILD_TYPE=Release -DCARES_STATIC=Yes -DCARES_SHARED=No -DCARES_BUILD_TOOLS=No -DCMAKE_INSTALL_PREFIX=$installdir ..
-emmake make -j4
+emcmake cmake CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' -DCMAKE_BUILD_TYPE=Release -DCARES_STATIC=Yes -DCARES_SHARED=No -DCARES_BUILD_TOOLS=No -DCMAKE_INSTALL_PREFIX=$installdir ..
+emmake make CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' -j4
 emmake make install
 cd ../..
 
@@ -54,8 +54,8 @@ cd ../..
 wget https://github.com/curl/curl/releases/download/curl-8_11_0/curl-8.11.0.zip
 unzip curl-8.11.0.zip
 cd curl-8.11.0
-CPPFLAGS="-I$installdir/include/" LDFLAGS="-L$installdir/lib -L$installdir/libx32" emconfigure ./configure --disable-shared --with-openssl --without-libpsl --disable-docs --with-ca-embed=/etc/ssl/certs/ca-certificates.crt --with-nghttp2 --enable-ares --prefix $installdir
-emmake make -j4
+CFLAGS="-pthread" CXXFLAGS="-pthread" CPPFLAGS="-I$installdir/include/" LDFLAGS="-L$installdir/lib -L$installdir/libx32 -sPTHREAD_POOL_SIZE=4" emconfigure ./configure --disable-shared --with-openssl --without-libpsl --disable-docs --with-ca-embed=/etc/ssl/certs/ca-certificates.crt --with-nghttp2 --enable-ares --prefix $installdir
+emmake make CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' -j4
 emmake make install
 cd ..
 
@@ -65,8 +65,8 @@ unzip SDL3-3.1.6.zip
 cd SDL3-3.1.6
 mkdir build
 cd build
-emcmake cmake -DSDL_STATIC=ON -DCMAKE_BUILD_TYPE=Release -DSDL_THREADS=On -DCMAKE_INSTALL_PREFIX=$installdir ..
-emmake make -j4
+emcmake cmake CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' -DSDL_STATIC=ON -DCMAKE_BUILD_TYPE=Release -DSDL_THREADS=On -DCMAKE_INSTALL_PREFIX=$installdir ..
+emmake make CFLAGS='-pthread' CXXFLAGS='-pthread' LD_FLAGS='-sPTHREAD_POOL_SIZE=4' -j4
 emmake make install
 cd ../..
 
